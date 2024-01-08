82211964a6ebb0a4393d428156212598ae1ca5fc
javi <jaravifa@hotmail.com>
2024-01-08 16:00:02 +0100

    Segundo commit

--- DOM/creandoElementos.js
+++ DOM/creandoElementos.js
@@ -9,7 +9,7 @@ const nuevaCaja = document.createElement('div');
     2. Agregamos texto y atributos
 */
 nuevaCaja.innerText = 'Nueva Caja!';
-nuevaCaja.setAttribute('id', 'nuevo-id');
+// nuevaCaja.setAttribute('id', 'nuevo-id');
 nuevaCaja.setAttribute('class', 'caja activa');
 
 /*
@@ -18,7 +18,25 @@ nuevaCaja.setAttribute('class', 'caja activa');
 const contenedor = document.getElementById('contenedor1');
 
 // .appendChild() - Agrega un elemento al final
-contenedor.appendChild(nuevaCaja);
+// contenedor.appendChild(nuevaCaja);
+
+// .prepend() - Agrega un elemento al principio
+// contenedor.prepend(nuevaCaja);
+
+/*
+    .insertAdjacentElement() - Nos permite agregar un elemnto en distintas posiciones.
+    Valores:
+        afterbegin - como primer elemento
+        beforebegin - antes del elemento padre
+        beforeend - como último elemento
+        afterend - despues del elemento padre
+*/
+// contenedor.insertAdjacentElement('afterbegin', nuevaCaja);
+
+// .replaceWith() - Nos permite reemplaza un elemento por otro
+document.querySelector('#contenedor1 .caja').replaceWith(nuevaCaja);
 
 };
+
+
 
